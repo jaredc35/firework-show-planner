258476a7b8d2c6ff42a2f60099eef2fccf9e5568
Jared Conway <jaredt.conway@gmail.com>
2026-02-16 08:33:14 +0000

    reconfigure config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config from environment variables
-// See .env.local for configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBW3Dd4pGO_GTzYz0RByNJZ-nWh4FY6TuQ",
+  authDomain: "richards-fireworks-60589.firebaseapp.com",
+  projectId: "richards-fireworks-60589",
+  storageBucket: "richards-fireworks-60589.firebasestorage.app",
+  messagingSenderId: "364668182372",
+  appId: "1:364668182372:web:04b2f4174d4a7ba2932471",
+  measurementId: "G-GVCVYJ5ZQG",
 };
 
 const app = initializeApp(firebaseConfig);
